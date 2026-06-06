5e977611434cdecb29bf3882757aee0ca5a27426
Andy Gu <gua6@uci.edu>
2026-06-05 23:56:55 -0700

    Updated MyAI.py to meet MinimumAI requirements.

--- Minesweeper_Python/src/MyAI.py
+++ Minesweeper_Python/src/MyAI.py
@@ -34,143 +34,258 @@ class MyAI(AI):
         # -1 to store unknown
         self.board = [[-1 for _ in range(rowDimension)] for _ in range(colDimension)]
 
-        # Tiles we have already proven safe but haven't uncovered yet.
-        self.safe_queue = deque()
+        # Tiles proven to be mines
+        self.known_mines = set()
 
-        # Tiles provably mines:
-        self.mines = set()
+        # Tiles already queued as safe
+        self.safe_set = set()
 
         # Tiles that we have seen (or are already queued to be seen)
-        self.queued = set()
+        self.safe_queue = deque()
 
         # Uncovered tiles whose number > 0
         self.frontier = set()
 
-        # Track where flags are
-        self.flagged = set()
-        self._last_action = None
-
         self.last_x = startX
         self.last_y = startY
-        self.queued.add((startX, startY))
+
+        # Track last action sent to game
+        self.last_action = AI.Action.UNCOVER
+
+        # Count safe tiles that were uncovered
+        self.uncovered_count = 0
 
     ########################################################################
     # YOUR CODE ENDS						   #
     ########################################################################
+    
     def getAction(self, number: int) -> Action:
-        if number == -1:
-            # If the last action was a flag/unflag, add/remove the tile to the flagged set
-            if self._last_action == AI.Action.FLAG:
-                self.flagged.add((self.last_x, self.last_y))
-            elif self._last_action == AI.Action.UNFLAG:
-                self.flagged.discard((self.last_x, self.last_y))
-        else:
-            # Record last move on board
-            self.board[self.last_x][self.last_y] = number
+        self.update_after_uncover(number)
 
-            # If tile is 0 it is def safe
-            if number == 0:
-                for dx, dy in POSITIONS:
-                    nx, ny = self.last_x + dx, self.last_y + dy
-                    if (
-                        0 <= nx < self.colDimension
-                        and 0 <= ny < self.rowDimension
-                        and (nx, ny) not in self.queued
-                    ):
-                        self.safe_queue.append((nx, ny))
-                        self.queued.add((nx, ny))
-            else:
-                # tile is a non-zero frontier tile (we are unsure right now)
-                self.frontier.add((self.last_x, self.last_y))
+        self.apply_basic_rules()
+        self.apply_global_rules()
+        
+        action = self.pop_safe_action()
+        if action is not None:
+            return action
 
-        # Clear all guaranteed safe tiles (to get to the full frontier)
-        while self.safe_queue:
-            x, y = self.safe_queue.popleft()
+        total_safe = self.rowDimension * self.colDimension - self.totalMines
+        if self.uncovered_count >= total_safe:
+            return Action(AI.Action.LEAVE)
 
-            res = self.uncover_tile(x, y)
-            # if tile is already uncovered, skip it
-            if not res:
-                continue
-            return res
-
-        # flag all provably mines
-        if self.mines:
-            x, y = self.mines.pop()
-            self.store_action(x, y, AI.Action.FLAG)
-            return Action(AI.Action.FLAG, x, y)
-
-        # continue code for frontier
-        for fx, fy in list(self.frontier):
-            res, flags = self.check_position(fx, fy)
-
-            # if all empty spots are mines...
-            num_empty = len(res)
-            mine_total = self.board[fx][fy]
-
-            # if there are no empty spots, remove from frontier (since we can't do anything with it)
-            if num_empty == 0:
-                self.frontier.discard((fx, fy))
-
-            mines_among_unknown = mine_total - flags
-            # all free positions are guaranteed to be free (mines among unknowns = 0)
-            if res and mines_among_unknown == 0:
-                for pos_tup in res:
-                    self.safe_queue.append(pos_tup)
-
-                # technically could be unsafe out if x,y is out of bounds..?
-                x, y = self.safe_queue.popleft()
-                return self.uncover_tile(x, y)
-
-            # all unknown positions are mines
-            if res and num_empty == mines_among_unknown:
-                for pos_tup in res:
-                    self.mines.add(pos_tup)
-
-                # technically could be unsafe out of bounds in case res is empty..?
-                x, y = res[0]
-                self.mines.discard((x, y))
-                self.store_action(x, y, AI.Action.FLAG)
-                return Action(AI.Action.FLAG, x, y)
-
-        # If we have flagged all mines, all remaining tiles can be assumed to be safe
-        if len(self.flagged) == self.totalMines:
-            for x in range(self.colDimension):
-                for y in range(self.rowDimension):
-                    if self.board[x][y] == -1 and (x, y) not in self.flagged:
-                        self.safe_queue.append((x, y))
+        # Guess if no guaranteed move exists
+        action = self.guess_tile()
+        if action is not None:
+            return action
 
         return Action(AI.Action.LEAVE)
+
+        
+    def update_after_uncover(self, number):
+        if number == -1:
+            return
+        
+        if self.board[self.last_x][self.last_y] == -1:
+            self.board[self.last_x][self.last_y] = number
+            self.uncovered_count += 1
+
+        if number == 0:
+            for nx, ny in self.neighbors(self.last_x, self.last_y):
+                self.add_safe(nx, ny)
+        else:
+            self.frontier.add((self.last_x, self.last_y))
+
+    def apply_basic_rules(self):
+        changed = True
+
+        while changed:
+            changed = False
+
+            for x, y in list(self.frontier):
+                if self.board[x][y] <= 0:
+                    self.frontier.discard((x, y))
+                    continue
+
+                unknowns = []
+                known_mine_count = 0
+
+                # Count unknown neighbors and known mines
+                for nx, ny in self.neighbors(x, y):
+                    if (nx, ny) in self.known_mines:
+                        known_mine_count += 1
+                    elif self.board[nx][ny] == -1:
+                        unknowns.append((nx, ny))
+
+                if not unknowns:
+                    self.frontier.discard((x, y))
+                    continue
+
+                remaining_mines = self.board[x][y] - known_mine_count
+
+                # If all mines are found, rest are safe
+                if remaining_mines <= 0:
+                    for x, y in unknowns:
+                        if self.add_safe(x, y):
+                            changed = True
+
+                # If all unknowns must be mines, mark them
+                elif remaining_mines == len(unknowns):
+                    for tile in unknowns:
+                        if tile not in self.known_mines:
+                            self.known_mines.add(tile)
+                            self.safe_set.discard(tile)
+                            changed = True
+
+
+    def apply_global_rules(self):
+        unknowns = self.all_unknown_tiles()
+        remaining_mines = self.totalMines - len(self.known_mines)
+
+        # If all mines are known, everything else is safe
+        if remaining_mines <= 0:
+            for x, y in unknowns:
+                self.add_safe(x, y)
+
+        # If every unknown tile is a mine, mark them
+        elif remaining_mines == len(unknowns):
+            for tile in unknowns:
+                self.known_mines.add(tile)
+                self.safe_set.discard(tile)
+
+
+    def guess_tile(self):
+        unknowns = self.all_unknown_tiles()
+
+        if not unknowns:
+            return None
+
+        remaining_mines = max(0, self.totalMines - len(self.known_mines))
+        global_risk = float(remaining_mines) / float(len(unknowns))
+
+        best_tile = None
+        best_risk = 2.0
+
+        for x, y in unknowns:
+            risks = [global_risk]
+
+            # Check nearby clues to estimate risk
+            for nx, ny in self.neighbors(x, y):
+                clue = self.board[nx][ny]
+
+                if clue <= 0:
+                    continue
+
+                unknown_neighbors = []
+                known_mine_count = 0
+
+                for ax, ay in self.neighbors(nx, ny):
+                    if (ax, ay) in self.known_mines:
+                        known_mine_count += 1
+                    elif self.board[ax][ay] == -1:
+                        unknown_neighbors.append((ax, ay))
+
+                remaining = clue - known_mine_count
+
+                if unknown_neighbors and remaining >= 0:
+                    risks.append(float(remaining) / float(len(unknown_neighbors)))
+
+            risk = max(risks)
+
+            # Small preference for edges/corners
+            if x == 0 or x == self.colDimension - 1:
+                risk -= 0.0001
+
+            if y == 0 or y == self.rowDimension - 1:
+                risk -= 0.0001
+
+            if risk < best_risk:
+                best_risk = risk
+                best_tile = (x, y)
+
+        if best_tile is None:
+            return None
+
+        return self.uncover_tile(best_tile[0], best_tile[1])
+
+
+    def pop_safe_action(self):
+        while self.safe_queue:
+            x, y = self.safe_queue.popleft()
+            self.safe_set.discard((x, y))
+
+            if self.board[x][y] == -1 and (x, y) not in self.known_mines:
+                return self.uncover_tile(x, y)
+
+        return None
+
+
+    def add_safe(self, x, y):
+        if not self.in_bounds(x, y):
+            return False
+
+        tile = (x, y)
+
+        if self.board[x][y] != -1:
+            return False
+
+        if tile in self.known_mines:
+            return False
+
+        if tile in self.safe_set:
+            return False
+
+        self.safe_set.add(tile)
+        self.safe_queue.append(tile)
+
+        return True
+
+
+    def all_unknown_tiles(self):
+        result = []
+
+        for x in range(self.colDimension):
+            for y in range(self.rowDimension):
+                if self.board[x][y] == -1 and (x, y) not in self.known_mines:
+                    result.append((x, y))
+
+        return result
+
+
+    def neighbors(self, x, y):
+        result = []
+
+        for dx, dy in POSITIONS:
+            nx = x + dx
+            ny = y + dy
+
+            if self.in_bounds(nx, ny):
+                result.append((nx, ny))
+
+        return result
+
+
+    def in_bounds(self, x, y):
+        return 0 <= x < self.colDimension and 0 <= y < self.rowDimension
+
 
     def store_action(self, x, y, action):
         self.last_x = x
         self.last_y = y
-        self._last_action = action
+        self.last_action = action
 
-    def check_position(self, x, y) -> list[list[tuple[int, int]], int]:
-        # single check function:
-        # Return positions of all free
-        result = []
-        flags = 0
-        for dx, dy in POSITIONS:
-            t_x = x + dx
-            t_y = y + dy
 
-            if (t_x, t_y) in self.flagged:
-                flags += 1
+    def uncover_tile(self, x, y):
+        if not self.in_bounds(x, y):
+            return None
 
-            elif (
-                0 <= t_x < self.colDimension
-                and 0 <= t_y < self.rowDimension
-                and self.board[t_x][t_y] == -1
-            ):
-                result.append((t_x, t_y))
-
-        return [result, flags]
-
-    def uncover_tile(self, x, y) -> Action | None:
         if self.board[x][y] != -1:
             return None
+
+        if (x, y) in self.known_mines:
+            return None
+
         self.store_action(x, y, AI.Action.UNCOVER)
+
         return Action(AI.Action.UNCOVER, x, y)
 
     ########################################################################
